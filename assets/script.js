0da64c4d9e64adc3e8a6ec89f9992b6ed87bd2cb
Joe <josephl@tuta.com>
2024-11-24 23:07:55 +0000

    Adding Images, js, and change text

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const footer = document.querySelector('footer p');
+footer.innerHTML = `© ${new Date().getFullYear()} Joseph Lavoie`;
