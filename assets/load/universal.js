ef92279d7220424b815f8eb9456d7ac596505ae0
Joe <josephl@tuta.com>
2024-11-29 03:52:45 +0000

    small location fix

--- assets/load/universal.js
+++ assets/load/universal.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('header.html')
+    fetch('load/header.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
